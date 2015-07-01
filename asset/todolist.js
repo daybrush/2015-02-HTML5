f8d4045f09340a16cc6ca97cea00e083b4cff1c5
byjo <buyongjo@gmail.com>
2015-07-01 18:56:14 +0900

    template lib - handlebars 사용

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,44 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-var inputTodo = document.getElementById('new-todo');
-var ulTodoList = document.getElementById('todo-list');
+	var inputTodo = document.getElementById('new-todo');
+	var ulTodoList = document.getElementById('todo-list');
 
-inputTodo.addEventListener('keypress', function(e) {
-	var key = e.keyCode;
-	if (key === 13) {
-		var msg = inputTodo.value;
-		addLi(msg);
+	var source = document.getElementById('todo-template').innerHTML;
+	var todoTemplate = Handlebars.compile(source);
+
+	inputTodo.addEventListener('keypress', addTodo);
+
+	function addTodo(e) {
+		var ENTER_KEYCODE = 13;
+		if (e.keyCode === ENTER_KEYCODE) {
+			var context = {todo: inputTodo.value};
+			var todo = todoTemplate(context);
+			ulTodoList.insertAdjacentHTML('beforeend', todo);
+			inputTodo.value = "";
+		}
 	}
-});
-
-function li() {
-	this.element = document.createElement('li');
-	this.element.classList.add('{}');
-
-	this.view = document.createElement('div');
-	this.view.classList.add('view');
-
-	this.inputb = document.createElement('input');
-	this.inputb.classList.add('toggle');
-	this.inputb.type = 'checkbox';
-
-	this.labelb = document.createElement('label');
-
-	this.button = document.createElement('button');
-	this.button.classList.add('destroy');
-
-	this.element.appendChild(this.view);
-	this.view.appendChild(this.inputb);
-	this.view.appendChild(this.labelb);
-	this.view.appendChild(this.button);
-}
-
-
-function addLi(msg) {
-	var newLi = new li();
-	newLi.labelb.innerText = msg;
-	ulTodoList.appendChild(newLi.element);
-	inputTodo.value = "";
-}
 
 });
