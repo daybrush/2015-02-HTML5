97280370be4bf8de2f6696bbb9bb3ab12b538310
JB <JB@JBui-MacBook-Pro.local>
2015-07-02 20:43:44 +0900

    week2 css transition

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
 	document.querySelector("#new-todo").addEventListener("keydown", addTodo); 
+	document.querySelector("#todo-list").addEventListener("click", completedTodo);
+	document.querySelector("#todo-list").addEventListener("click", removeTodo);
 })
 
 function addTodo(e){
@@ -8,6 +10,9 @@ function addTodo(e){
 		var todo = makeTodo(document.querySelector("#new-todo").value);
 		document.querySelector("#todo-list").appendChild(todo);
 		document.querySelector("#new-todo").value = "";
+		setTimeout(function() {
+    		todo.classList.add("appending");
+  		}, 10);
 	}
 }
 
@@ -19,18 +24,36 @@ function makeTodo(title){
 	return li;
 }
 
+function completedTodo(e){
+	var input = e.target;
+	var li = e.target.parentNode.parentNode;
+	if(e.target.className !== "toggle") return;
+	if(input.checked){
+		li.className = "completed";
+	}else{
+		li.className = "";
+	}
+}
+
+function removeTodo(e){
+	var destroy = e.target;
+	var li = e.target.parentNode.parentNode;
+	if(e.target.className !== "destroy") return;
+  	li.classList.add("deleting");
+	setTimeout(function() {
+  		li.parentNode.removeChild(li);
+  	}, 400);
+}
+
 function Todo(title){
 	this.title = title;
 }
 
 Templates = {}; 
 Templates.todoTemplate = [
-    '<li>',
 		'<div class="view">',
 			'<input class="toggle" type="checkbox">',
 			'<label>{{title}}</label>',
 			'<button class="destroy"></button>',
 		'</div>',
-	'</li>'
 ].join("\n");
-
