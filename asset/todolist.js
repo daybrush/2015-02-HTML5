0e086f907127c55fb678182979d0aee5865689f3
JB <JB@JBui-MacBook-Pro.local>
2015-07-03 10:48:08 +0900

    css checked bug 수정, js 코드 개선

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,52 +1,49 @@
-document.addEventListener("DOMContentLoaded", function(){
-	document.querySelector("#new-todo").addEventListener("keydown", addTodo); 
-	document.querySelector("#todo-list").addEventListener("click", completedTodo);
-	document.querySelector("#todo-list").addEventListener("click", removeTodo);
-})
-
-function addTodo(e){
-	var ENTER_KEYCODE = 13;
-	if(e.keyCode === ENTER_KEYCODE) {
-		var todo = makeTodo(document.querySelector("#new-todo").value);
-		document.querySelector("#todo-list").appendChild(todo);
-		document.querySelector("#new-todo").value = "";
-		setTimeout(function() {
+var TODO = {
+	ENTER_KEYCODE : 13,
+	init : function(){
+		document.addEventListener("DOMContentLoaded", function(){
+			document.querySelector("#new-todo").addEventListener("keydown", this.add.bind(this)); 
+			document.querySelector("#todo-list").addEventListener("click", this.completed);
+			document.querySelector("#todo-list").addEventListener("click", this.remove);
+		}.bind(this));
+	},
+	build : function(todo){
+		var li = document.createElement("li");
+		var todoObj = { title : todo }
+		var template = Handlebars.compile(Templates.todoTemplate);
+		li.innerHTML = template(todoObj);
+		return li;
+	},
+	add : function(e){
+		if(e.keyCode === this.ENTER_KEYCODE) {
+			var todo = this.build(document.querySelector("#new-todo").value);
+			document.querySelector("#todo-list").appendChild(todo);
+			document.querySelector("#new-todo").value = "";
+			setTimeout(function() {
     		todo.classList.add("appending");
-  		}, 10);
+  			}, 10);
+  		}
+	},
+	completed : function(e){
+		var input = e.target;
+		var li = e.target.parentNode.parentNode;
+		if(e.target.className !== "toggle") return;
+		if(input.checked){
+			li.className = "completed";
+		}else{
+			li.className = "";
+		}
+
+	}, 
+	remove : function(e){
+		var destroy = e.target;
+		var li = e.target.parentNode.parentNode;
+		if(e.target.className !== "destroy") return;
+  		li.classList.add("deleting");
+		setTimeout(function() {
+  			li.parentNode.removeChild(li);
+  		}, 1000);
 	}
-}
-
-function makeTodo(title){
-	var li = document.createElement("li");
-	var todoObj = new Todo(title);
-	var template = Handlebars.compile(Templates.todoTemplate);
-	li.innerHTML = template(todoObj);
-	return li;
-}
-
-function completedTodo(e){
-	var input = e.target;
-	var li = e.target.parentNode.parentNode;
-	if(e.target.className !== "toggle") return;
-	if(input.checked){
-		li.className = "completed";
-	}else{
-		li.className = "";
-	}
-}
-
-function removeTodo(e){
-	var destroy = e.target;
-	var li = e.target.parentNode.parentNode;
-	if(e.target.className !== "destroy") return;
-  	li.classList.add("deleting");
-	setTimeout(function() {
-  		li.parentNode.removeChild(li);
-  	}, 400);
-}
-
-function Todo(title){
-	this.title = title;
 }
 
 Templates = {}; 
@@ -57,3 +54,5 @@ Templates.todoTemplate = [
 			'<button class="destroy"></button>',
 		'</div>',
 ].join("\n");
+
+TODO.init();
