5290c0b50c9c80246de38f78bf78e9f44e6238db
milooy <milooy@icloud.com>
2015-07-05 06:42:45 +0900

    [week2] change setInterval animation to CSS animation

--- asset/todolist.js
+++ asset/todolist.js
@@ -6,16 +6,6 @@ function addTODO(e) {
         var template = Handlebars.compile(source);
         var data = { todo : $('#new-todo').val()};
         $("#todo-list").append(template(data));
-
-        var li = $('#todo-list li:last-child').css('opacity', 0);
-        var i = 0;
-        var key = setInterval(function() {
-            if(i===50){clearInterval(key);}
-            // TODO: js opacity와 jquery opacity가 다른가? 0.02하면 안된다.
-            else {li.css('opacity', i*0.2)};
-            i++;
-        }, 12);
-
         $('#new-todo').val("");
     }
 }
@@ -26,14 +16,13 @@ function completedTODO(e){
 
 function removeTODO(e){
     var li = $(e.currentTarget).closest('li');
-    var i = 0;
-    var key = setInterval(function() {
-        if(i===50){
-            clearInterval(key);
+    if (!$(this).hasClass('disabled')) {
+        li.css('animation', 'fadeOut .5s');
+        li.on('animationend || webkitAnimationEnd',function(){
             li.remove();
-        } else {li.css('opacity', 1-i*0.2)};
-        i++;
-    }, 12);
+            $(this).toggleClass('disabled');
+        });
+    }
 }
 
 $(function() {
