f7720e389ed9420b59cbba650a71268a83523370
helloheesu <heesu@nhnnext.org>
2015-07-04 16:28:23 +0900

    addClass at js, not at html / delete dirty comments

--- asset/todolist.js
+++ asset/todolist.js
@@ -5,6 +5,7 @@
 * 클릭이미됐으면 무시
 * TODO.removeItem
 * 아무것도없을때 입력칸 혹은 맨마지막 아이템, 위에 희미하게 회색선. 뭐지?
+* 사라지는 모습이 좀 못 생김. 애니메이션 끝 - 사라짐 연결이 부드럽지 않음. 근데 그 전에도 그랬네.
 */
 TODO = {
   item : null,
@@ -19,29 +20,12 @@ TODO.init = function () {
 TODO.addItem = function (sContents) {
   var data = {title:sContents};
   TODO.board.append(TODO.template(data));
+  var lastLi = $('li:last-child');
+  lastLi.addClass('appending');
   // ????? setTimeout 안 하면 왜 transition 이 안 먹히지
   setTimeout(function () {
-    $('li:last-child').removeClass('appending');
+    lastLi.removeClass('appending');
   }, 10);
-  // 어차피 바로 먹힐 거 'appending' 이라는 클래스를 템플릿에 써 두지 말고
-  // 여기서 추가했다가 삭제하는 걸로 할까.
-
-  /* setTimeout 대신 requestAnimationFrame, 이렇게까지???
-  * http://stackoverflow.com/questions/12088819/css-transitions-on-new-elements
-  * 위 스택오버플로 글에서 파폭에서는 setTimeout 잘 안된다고. 근데 해봤는데 잘됨;;;
-
-  // var start;
-  // function step(timestamp) {
-  //   if (!start) start = timestamp;
-  //   if (timestamp - start < 10) {
-  //     window.requestAnimationFrame(step);
-  //   } else {
-  //     $('li:last-child').removeClass('appending');
-  //   }
-  // }
-  // window.requestAnimationFrame(step);
-
-  */
 };
 
 $(document).ready(function () {
@@ -63,12 +47,7 @@ $(document).ready(function () {
     cachedLi.addClass('deleting');
     var removeItem = function () {
       cachedLi.remove();
-      // 사라지는 모습이 좀 못 생김. 애니메이션 끝 - 사라짐 연결이 부드럽지 않음. 근데 그 전에도 그랬네.
-
-      // cachedLi.off('transitionend', removeItem);
-      // remove 가 알아서 해 주니까 필요없음.
     };
     cachedLi.on('webkitTransitionEnd transitionend', removeItem);
-    // https://developer.mozilla.org/ko/docs/Web/Guide/CSS/Using_CSS_transitions#.ED.8A.B8.EB.9E.9C.EC.A7.80.EC.85.98_.EC.99.84.EB.A3.8C_.EA.B0.90.EC.A7.80.ED.95.98.EA.B8.B0
   });
 });
