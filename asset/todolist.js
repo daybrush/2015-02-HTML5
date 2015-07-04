f4bc9567b3ccab95286da54428c189363f753424
helloheesu <heesu@nhnnext.org>
2015-07-04 16:49:34 +0900

    삭제중인 아이템은 삭제클릭 무시하기

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,9 +1,10 @@
 /*
-* transition, transform
-  * 개발자도구로 opacity 바꿔놓으면 destroy가 잘 안 되는 문제가 있음.
-* new TODO() 는 굳이 ㄴㄴ. 어차피 딱 하나인데. 차라리 new Item().
-* 클릭이미됐으면 무시
+* done
 * TODO.removeItem
+* destroy, 클릭이미됐으면 그다음클릭은 무시하기.
+* todo
+* transition으로 애니메이션 하면, 개발자도구로 opacity 바꿔놓으면 destroy가 잘 안 되는 문제가 있음.
+* new TODO() 는 굳이 ㄴㄴ. 어차피 딱 하나인데. 차라리 new Item().
 * 아무것도없을때 입력칸 혹은 맨마지막 아이템, 위에 희미하게 회색선. 뭐지?
 * 사라지는 모습이 좀 못 생김. 애니메이션 끝 - 사라짐 연결이 부드럽지 않음. 근데 그 전에도 그랬네.
 */
@@ -27,6 +28,15 @@ TODO.addItem = function (sContents) {
     lastLi.removeClass('appending');
   }, 10);
 };
+TODO.removeItem = function (cachedLi) {
+  console.log(cachedLi); // 무시하기 잘 됐나 확인.
+  if (!cachedLi) return;
+  cachedLi.addClass('deleting');
+  var removeItem = function () {
+    cachedLi.remove();
+  };
+  cachedLi.on('webkitTransitionEnd transitionend', removeItem);
+}
 
 $(document).ready(function () {
   TODO.init();
@@ -42,12 +52,7 @@ $(document).ready(function () {
     var checked = $(this).is(':checked');
     $(this).closest('li').toggleClass('completed', checked);
   });
-  $('#todo-list').on('click', 'button.destroy', function() {
-    var cachedLi = $(this).closest('li');
-    cachedLi.addClass('deleting');
-    var removeItem = function () {
-      cachedLi.remove();
-    };
-    cachedLi.on('webkitTransitionEnd transitionend', removeItem);
+  $('#todo-list').on('click', 'li:not(.deleting) button.destroy', function() {
+    TODO.removeItem($(this).closest('li'));
   });
 });
