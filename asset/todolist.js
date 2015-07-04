f2ca009661805b09fad38f7517a8b1e30a7c229a
helloheesu <heesu@nhnnext.org>
2015-07-04 11:23:58 +0900

    fadeIn animation

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,7 +1,5 @@
 /*
-* Handlebars 사용 방식 변경
 * TODO.removeItem
-* .addItem 할때 애니메이션
 * 클릭이미됐으면 무시
 * requestAnimationFrame
 * transition, transform
@@ -20,6 +18,25 @@ TODO.init = function () {
 TODO.addItem = function (sContents) {
   var data = {title:sContents};
   TODO.board.append(TODO.template(data));
+
+  var cacheDiv = $('li:last-child', TODO.board).find('div');
+  // 심지어 여기도 li 아니고 div.
+  cacheDiv.css('opacity', 0);
+  var _fadeIn = setInterval(function () {
+
+    var prev = cacheDiv.css('opacity');
+    console.log(prev, typeof(prev));
+    var prev = parseFloat(cacheDiv.css('opacity'));
+    console.log(prev, typeof(prev));
+    if(typeof(prev) !== "number") prev = 0;
+    if(prev >= 1) {
+      console.log('end!');
+      clearInterval(_fadeIn);
+      // 항상 마지막 값이 1.1. 왜?
+    }
+    console.log(prev+0.1);
+    cacheDiv.css('opacity', prev+0.1);
+  }, 30); // 0.3초 만에 끝나게 하려면, 30ms(==0.03초)마다 0.1씩.
 };
 
 $(document).ready(function () {
@@ -41,11 +58,13 @@ $(document).ready(function () {
     // li로 하면 매우 오랜 시간이 걸려서 fadeOut 됨.
     // 심지어 그 속도가 내 숫자와 상관없이 상수인 듯;
     var _fadeOut = setInterval(function () {
-      var prev = cacheDiv.css('opacity');
-      if(typeof(prev) != "string") prev = 1;
+      var prev = parseFloat(cacheDiv.css('opacity'));
+      if(typeof(prev) !== "number") prev = 1;
       if(prev <= 0) {
         console.log('end!');
         clearInterval(_fadeOut);
+        // 여기도 마지막 값이 -0.1. ????
+        // 아니다. X버튼 여러개 눌러서 정신없게하면 0.2도.
         console.log(cacheDiv.closest('li').remove());
       }
       // console.log(prev, typeof(prev));
