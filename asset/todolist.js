ba437beb92d3c23e082e918dc91629ab67ccd9d1
milooy <milooy@icloud.com>
2015-07-04 18:48:43 +0900

    [week1] add handlebar, change todolist.js using handlebar.js

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,33 +1,14 @@
-function makeTODO(todo) {
-    var li = document.createElement('li');
-    var div = document.createElement('div');
-    div.className = "view";
-    var input = document.createElement('input');
-    input.className = "toggle";
-    input.setAttribute("type", "checkbox")
-    var label = document.createElement('label');
-    label.innerText = todo;
-    var button = document.createElement('button');
-    button.className = "destroy";
-
-    div.appendChild(input);
-    div.appendChild(label);
-    div.appendChild(button);
-    li.appendChild(div);
-
-    return li;
-}
-
 function addTODO(e) {
     var ENTER_KEYCODE = 13;
     if($('#new-todo').val() && e.keyCode === ENTER_KEYCODE) {
-        var todo = makeTODO($('#new-todo').val());
-
-        $("#todo-list").append(todo);
+        var source   = $("#todo-template").html();
+        var template = Handlebars.compile(source);
+        var data = { todo : $('#new-todo').val()};
+        $("#todo-list").append(template(data));
         $('#new-todo').val("");
     }
 }
 
 $(function() {
-    $('#new-todo').keydown(addTODO);
+  $('#new-todo').keydown(addTODO);
 });
