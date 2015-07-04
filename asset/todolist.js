850f7ae521f895fb616d9877a1574d6ba70af872
helloheesu <heesu@nhnnext.org>
2015-07-04 14:58:46 +0900

    requestAnimationFrame
    https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,5 +1,4 @@
 /*
-* requestAnimationFrame
 * transition, transform
 * new TODO() 는 굳이 ㄴㄴ. 어차피 딱 하나인데. 차라리 new Item().
 * 클릭이미됐으면 무시
@@ -21,16 +20,18 @@ TODO.addItem = function (sContents) {
 
   var cacheDiv = $('li:last-child', TODO.board).find('div');
   cacheDiv.css('opacity', 0);
-  var _fadeIn = setInterval(function () {
 
-    var prev = cacheDiv.css('opacity');
-    var prev = parseFloat(cacheDiv.css('opacity'));
-    if(typeof(prev) !== "number") prev = 0;
-    if(prev >= 1) {
-      clearInterval(_fadeIn);
+  var start;
+  var duration = 300; // [ms]
+  function step(timestamp) {
+    if (!start) start = timestamp;
+    var progress = timestamp - start; // [ms]
+    cacheDiv.css('opacity', progress/duration);
+    if (progress < duration) {
+      window.requestAnimationFrame(step);
     }
-    cacheDiv.css('opacity', prev+0.1);
-  }, 30); // 0.3초 만에 끝나게 하려면, 30ms(==0.03초)마다 0.1씩.
+  }
+  window.requestAnimationFrame(step);
 };
 
 $(document).ready(function () {
@@ -49,15 +50,20 @@ $(document).ready(function () {
   });
   $('#todo-list').on('click', 'button.destroy', function() {
     var cacheDiv = $(this).closest('div');
-    var _fadeOut = setInterval(function () {
-      var prev = parseFloat(cacheDiv.css('opacity'));
-      if(typeof(prev) !== "number") prev = 1;
-      if(prev <= 0) {
+
+    var start;
+    var duration = 300; // [ms]
+    function step(timestamp) {
+      if (!start) start = timestamp;
+      var progress = timestamp - start; // [ms]
+      cacheDiv.css('opacity', 1 - progress/duration);
+      if (progress < duration) {
+        window.requestAnimationFrame(step);
+      } else {
         console.log(cacheDiv);
-        clearInterval(_fadeOut);
         cacheDiv.closest('li').remove()
       }
-      cacheDiv.css('opacity', prev-0.1);
-    }, 30); // 0.3초 만에 끝나게 하려면, 30ms(==0.03초)마다 0.1씩.
+    }
+    window.requestAnimationFrame(step);
   });
 });
