62f16c937b9403ea479511c45f86ecacf056454a
minhyeok4dev <minhyeok4dev@gmail.com>
2015-07-05 18:43:04 +0900

    todo mission 2

--- asset/todolist.js
+++ asset/todolist.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 var todos = {
   init : function(){
     this.onKeyEvent();
+    this.onClickEvent();
   },
 
   onKeyEvent : function(){
@@ -14,6 +15,21 @@ var todos = {
         $('#new-todo').val("");
       }
     }.bind(this));
+  },
+
+  onClickEvent : function(){
+    $('#todo-list').on('click', function(e){
+      var target = $(e.target);
+
+      if (target.is('input.toggle')){
+        target.parents('li').toggleClass('completed');
+      } else if (target.is('button.destroy')){
+        target.parents('li').css('-webkit-animation', 'fadeOut 500ms');
+        target.parents('li').bind('webkitAnimationEnd',function(){
+            target.parents('li').remove();
+        });
+      }
+    });
   },
 
   newTODO : function(text){
