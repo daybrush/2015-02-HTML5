52a86c5e59b576fa45b99c5667a2e59a842b6dff
JongSung <jsfumato@naver.com>
2015-07-08 02:09:19 +0900

    주석 정리

--- todo.js
+++ todo.js
@@ -6,46 +6,12 @@ var ENTER_KEYCODE = 13;
 // 연결이 완료된 객체 중 가장 상위 객체인 li를 반환한다.
 
 
-/*
-function makeTODO(todo) {
-    "use strict";
-
-    // li ++  
-    var li = document.createElement("li");
-    var div = document.createElement("div");
-    div.className = "view";
-            
-    var input = document.createElement("input");
-    input.className = "toggle";
-    input.setAttribute("type", "checkbox");
-    input.addEventListener("click",todoCompleted)
-        
-    var label = document.createElement("label");
-    label.innerText = todo;
-    //todo는 document.getElementById("new-todo").value를 말한다.
-                    
-    var button = document.createElement("button");
-    button.className = "destroy";
-            
-    // DOM connect
-    div.appendChild(input);
-    div.appendChild(label);
-    div.appendChild(button);
-            
-    li.appendChild(div);
-    
-    return li;
-    //todo-list에 li를 appendChild 해야하기때문에 li Node를 반납함
-
-}
-*/
 var inputTodo = { newtodo : [] };
 
 function addTODO(e) {
     "use strict";
     if (e.keyCode === ENTER_KEYCODE) {
-
-       //template 시도
+       //template
         var template = $("#todo-template").html();
         var todoCompile = Handlebars.compile(template);
         var todo = document.getElementById("new-todo").value;
@@ -55,12 +21,6 @@ function addTODO(e) {
         
         $("#todo-list").html(todolist);
         document.getElementById("new-todo").value = "";
-
-/*
-        var todo = makeTODO(document.getElementById("new-todo").value);
-        document.getElementById("todo-list").appendChild(todo);
-        document.getElementById("new-todo").value = "";
-*/
     }
 }
 
