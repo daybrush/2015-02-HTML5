e7d9d91500e1ea917cc685bb628a7910401ec51d
JB <JB@JBui-MacBook-Pro.local>
2015-07-07 17:50:24 +0900

    insertAdjacentHTML 사용

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,3 +1,24 @@
+var TODOSync = {
+	get : function(){
+
+	}, 
+	add : function(){
+		var xhr = new XMLHttpRequest();
+		xhr.open("PUT", "http://ui.nhnnext.org:3333/JB1021",true);
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+		xhr.addEventListener("load", function(e){
+			callback(xhr.responseText);
+		});
+		xhr.send();
+	}, 
+	completed : function(){
+
+	}, 
+	remove : function(){
+
+	}
+}
+
 var TODO = {
 	ENTER_KEYCODE : 13,
 	init : function(){
@@ -8,20 +29,22 @@ var TODO = {
 		}.bind(this));
 	},
 	build : function(todo){
-		var li = document.createElement("li");
 		var todoObj = { title : todo }
 		var template = Handlebars.compile(Templates.todoTemplate);
-		li.innerHTML = template(todoObj);
-		return li;
+		console.log(template(todoObj));
+		return template(todoObj);
 	},
 	add : function(e){
 		if(e.keyCode === this.ENTER_KEYCODE) {
-			var todo = this.build(document.querySelector("#new-todo").value);
-			document.querySelector("#todo-list").appendChild(todo);
-			document.querySelector("#new-todo").value = "";
-			setTimeout(function() {
-    		todo.classList.add("appending");
-  			}, 10);
+			var todo = document.querySelector("#new-todo").value;
+			// TODOSync.add(todo, function(json){
+				var todoLi = this.build(todo);
+				document.querySelector("#todo-list").insertAdjacentHTML('beforeend', todoLi);
+				document.querySelector("#new-todo").value = "";
+				setTimeout(function() {
+    				document.querySelector("#todo-list").firstChild.classList.add("appending");
+  				}, 10);
+			// }.bind(this));
   		}
 	},
 	completed : function(e){
@@ -48,11 +71,13 @@ var TODO = {
 
 Templates = {}; 
 Templates.todoTemplate = [
+	'<li>',
 		'<div class="view">',
 			'<input class="toggle" type="checkbox">',
 			'<label>{{title}}</label>',
 			'<button class="destroy"></button>',
 		'</div>',
+	'<li>'
 ].join("\n");
 
 TODO.init();
