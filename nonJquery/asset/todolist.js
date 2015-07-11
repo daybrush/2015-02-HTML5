86074dcaa9a8dfca16110236a61f7a868d3bd85a
ho <hataeho1@naver.com>
2015-07-11 18:34:25 +0900

    [mission 2] - BaseCode + EventDeelegation

--- nonJquery/asset/todolist.js
+++ nonJquery/asset/todolist.js
@@ -5,7 +5,50 @@
 
 	document.addEventListener("DOMContentLoaded", function(){
 		document.getElementById("new-todo").addEventListener("keydown", addTodo);
+		document.getElementById("todo-list").addEventListener("click", completeTodo);
+		document.getElementById("todo-list").addEventListener("click", removeTodo);
 	});
+
+	function completeTodo(e) {
+		var target = e.target;
+		if(target.nodeName !== "INPUT" || target.className !== "toggle") {
+			return;
+		}
+
+		var checkBtn = target;
+		var li = checkBtn.parentNode.parentNode;
+
+		if(checkBtn.checked) {
+			li.classList.add("completed");
+		} else {
+			li.classList.remove("completed");
+		}
+	}
+
+	function removeTodo(e) {
+		var target = e.target;
+		if(target.nodeName !== "BUTTON" || target.className !== "destroy") {
+			return;
+		}
+
+		var destroyBtn = target;
+		var li = destroyBtn.parentNode.parentNode;
+
+		li.style.height = parseInt(window.getComputedStyle(destroyBtn).height) + "px";
+		li.style.overflow = "hidden";
+
+		function animate(timestamp) {
+			li.style.height = parseFloat(li.style.height) - 2 + "px";
+
+			if (parseFloat(li.style.height) > 0) {
+    			window.requestAnimationFrame(animate);
+  			} else {
+				li.parentNode.removeChild(li);		
+  			}
+		}
+
+		window.requestAnimationFrame(animate);
+	}
 
 	function makeTodo(sTodoMessage) {
 		if(sTodoMessage === "") {
@@ -31,8 +74,20 @@
 
 			var todoList = document.getElementById("todo-list");
 			todoList.insertAdjacentHTML("beforeend", sTodoEle);
-
 			e.target.value = "";
+
+			var lastEle = todoList.querySelector("li:nth-last-of-type(1)");
+			lastEle.style.opacity = 0;
+
+			function animate(timestamp) {
+				lastEle.style.opacity = lastEle.style.opacity*1 + 0.1;
+
+				if (lastEle.style.opacity< 1) {
+	    			window.requestAnimationFrame(animate);
+	  			}
+			}
+
+			window.requestAnimationFrame(animate);
 		}
 	}
 
