1df27bceab0ae2ca1e51cf796c08428843e8c15e
ho <hataeho1@naver.com>
2015-07-11 22:28:22 +0900

    [mission 2] other students code - shorter code

---
+++ etc/mission2 - shortcode/[minhyeok4dev]todolist.js
@@ -0,0 +1,42 @@
+// NHNNEXT 1기 김민혁님 코드
+
+$(document).ready(function(){
+  todos.init();
+});
+
+var todos = {
+  init : function(){
+    this.onKeyEvent();
+    this.onClickEvent();
+  },
+
+  onKeyEvent : function(){
+    $('#new-todo').keydown(function(e){
+      if (e.which == 13){
+        $('#todo-list').append(this.newTODO($('#new-todo').val()));
+        $('#new-todo').val("");
+      }
+    }.bind(this));
+  },
+
+  onClickEvent : function(){
+    $('#todo-list').on('click', function(e){
+      var target = $(e.target);
+
+      if (target.is('input.toggle')){
+        target.parents('li').toggleClass('completed');
+      } else if (target.is('button.destroy')){
+        target.parents('li').css('-webkit-animation', 'fadeOut 500ms');
+        target.parents('li').bind('webkitAnimationEnd',function(){
+            target.parents('li').remove();
+        });
+      }
+    });
+  },
+
+  newTODO : function(text){
+    var template = Handlebars.compile($('#new-todo-template').html());
+
+    return template({body: text});
+  }
+}
