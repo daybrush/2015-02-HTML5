4020e4d09d3d6268d9668fedffb901c1860ef631
ho <hataeho1@naver.com>
2015-07-11 14:31:39 +0900

    Apply handlebars template library

--- nonJquery/asset/todolist.js
+++ nonJquery/asset/todolist.js
@@ -1,59 +1,45 @@
-var ENTER_KETCODE = 13;
+(function(){
+	var ENTER_KETCODE = 13;
 
-document.addEventListener("DOMContentLoaded", function(){
-	document.getElementById("new-todo").addEventListener("keydown", addTodo);
-});
+	document.addEventListener("DOMContentLoaded", function(){
+		document.getElementById("new-todo").addEventListener("keydown", addTodo);
+	});
 
-function makeTodo(sTodoMessage) {
-	if(sTodoMessage === "") {
-		throw new EmptyStringError("missing Todo Message");
-	}
-
-	var document = window.document;
-
-	var li = document.createElement("li");
-	
-	var div = document.createElement("div");
-	div.className = "view";
-
-	var input = document.createElement("input");
-	input.className = "toggle";
-	input.setAttribute("type", "checkbox");
-
-	var label = document.createElement("label");
-	label.innerText = sTodoMessage;
-
-	var button = document.createElement("button");
-	button.className = "destroy";
-
-	div.appendChild(input);
-	div.appendChild(label);
-	div.appendChild(button);
-
-	li.appendChild(div);
-
-	return li;
-}
-
-function addTodo(e) {
-	if(e.keyCode === ENTER_KETCODE) {
-		var document = window.document;
-
-		try{
-			var elTodo = makeTodo(document.getElementById("new-todo").value);
-		} catch(err) {
-			alert(err.message);
-			return;
+	function makeTodo(sTodoMessage) {
+		if(sTodoMessage === "") {
+			throw new EmptyStringError("missing Todo Message");
 		}
-		
-		document.getElementById("todo-list").appendChild(elTodo);
-		document.getElementById("new-todo").value = "";
+
+		var source = document.getElementById("Todo-template").innerHTML;
+		var template = Handlebars.compile(source);
+
+		var context = {todoMessage : sTodoMessage};
+		var sHtml = template(context);
+		return sHtml;
 	}
-}
 
-function EmptyStringError(sMessage) {
-	this.name = "EmptyStringError";
-	this.message = sMessage;
-}
+	function addTodo(e) {
+		if(e.keyCode === ENTER_KETCODE) {
+			var document = window.document;
 
-EmptyStringError.protoType = new Error();
+			try{
+				var sTodoEle = makeTodo(e.target.value);
+			} catch(err) {
+				alert(err.message);
+				return;
+			}
+
+			var todoList = document.getElementById("todo-list");
+			todoList.insertAdjacentHTML("beforeend", sTodoEle);
+
+			e.target.value = "";
+		}
+	}
+
+	function EmptyStringError(sMessage) {
+		this.name = "EmptyStringError";
+		this.message = sMessage;
+	}
+
+	EmptyStringError.protoType = new Error();
+})();
