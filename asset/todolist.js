aad35e0b5fa3b332dcd8b04ccfa64ffc93a5837b
FreshFleshFlash <pharmacystic@gmail.com>
2015-07-12 20:03:56 +0900

    second

--- asset/todolist.js
+++ asset/todolist.js
@@ -2,33 +2,11 @@
 
 var KEYCODE_ENTER = 13;
 
-// function makeTodo(todo) {
-// 	var li = document.createElement("li");
-// 	var div = document.createElement("div");
-// 	div.className = "view";
-// 	var input = document.createElement("input");
-// 	input.className = "toggle";
-// 	input.setAttribute("type", "checkbox");
-// 	var label = document.createElement("label");
-// 	label.innerText = todo;
-// 	var button = document.createElement("button");
-// 	button.className = "destroy";
-
-// 	div.appendChild(input);
-// 	div.appendChild(label);
-// 	div.appendChild(button);
-
-// 	li.appendChild(div);
-
-// 	return li;
-// }
-
 function makeTodo(todo) {
 	var source = $("#todo-template").html();
 	var template = Handlebars.compile(source);
-	var context = {title: todo};
+	var context = {doWhat: todo};
 	var html = template(context);
-
 	return html;
 }
 
@@ -36,11 +14,30 @@ function addTodo(e) {
 	if(e.keyCode === KEYCODE_ENTER) {
 		var li = makeTodo($("#new-todo").val());
 		$("#todo-list").append(li);
-		$("#new-todo").val("");
+		$("#new-todo").val("");	
 	}
 }
 
-//$(document).bind("DOMContentLoaded", function() {
+function completeTodo(e) {
+	var input = e.target;
+	var li = e.target.parentNode.parentNode;
+	if(input.checked) {
+		li.className = "completed";
+	} else {
+		li.className = "";
+	}
+}
+
+function removeTodo(e) {
+	var li = e.target.parentNode.parentNode;
+	li.className = "deleting";
+	li.addEventListener('webkitTransitionEnd', function(e) { 
+		li.parentNode.removeChild(li);
+	}, false );
+}
+
 $(document).ready(function() {
 	$("#new-todo").bind("keydown", addTodo);
+	$("#todo-list").on("click", ".toggle", completeTodo);
+	$("#todo-list").on("click", ".destroy", removeTodo);
 });
