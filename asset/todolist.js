d9148526348efc076b2a94ed55401137b95aa2e4
cca-company <cca_company@naver.com>
2015-07-14 01:09:44 +0900

    2주차 과제

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,14 +1,17 @@
 var listnum = 0;
 
 // 엔터 이벤트 발생시
-$("#new-todo").bind("keydown",function(e){
+$("#new-todo").on("keydown",function(e){
 	if(e.keyCode == 13){
 		// 새 리스트 항목 생성
 		var title = $(this).val();
-		
-		$("li.sample").clone().appendTo($("#todo-list"))
+		var newList = $("li.sample").clone();
+		newList.prependTo($("#todo-list"))
+			.css({opacity:0})
 			.removeClass("sample")
 			.find("label").text(title);
+
+		newList.animate({opacity:1.0},400);
 
 		listnum++;
 		
@@ -16,6 +19,28 @@ $("#new-todo").bind("keydown",function(e){
 		$(this).val(null);
 	}
 });
+
+// 할일 체크
+$("body").on("click", ".toggle", function(){
+	var list = $(this).parent().parent();
+	if($(this).is(":checked")){
+		list.addClass("completed");
+	}else{
+		list.removeClass("completed");
+	}
+})
+
+// 할일 삭제
+$("body").on("click", ".destroy", function(){
+	var list = $(this).parent().parent();
+
+	list.animate({
+		height:'0px'	
+	},400,function(){
+		list.remove();	
+	});
+})
+
 
 var source = $("body").html();
 var template = Handlebars.compile(source);
