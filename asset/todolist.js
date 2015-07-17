a3581b3d9be6154bc90000f797c8d4114cc7dadf
Dahye Kim <dahye.kim@nhnnext.org>
2015-07-17 19:49:07 +0900

    innerHTML to insertAdjacentHTML

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,10 +1,14 @@
-/*<li class="{}">
-	<div class="view">
-		<input class="toggle" type="checkbox" {}>
-		<label>타이틀</label>
-		<button class="destroy"></button>
-	</div>
-</li>
+/*todo
+* innerHTML + = todo 로 처리했던 부분 insertAdjacentHTML 로 바꾸기
+* 등록한 할 일을 완료 처리하기
+*  - 이벤트 할당하기
+*  - class추가하기(li에 completed)
+* 삭제하기
+*  - 이벤트 할당하기
+*  - li을 서서히 사라지게 처리한 후 삭제
+* 등록하기
+*  - 애니메이션 기능을 추가
+
 */
 
 (function () {
@@ -13,12 +17,15 @@
 		newtodo.addEventListener("keydown", addTodo);
 	})
 
+	//var input = document.querySelector(".toggle");
+	//input[0].addEventListener("click", completeTodo);
+
 	function addTodo(ev) {
 		var ENTER_KEYCODE = 13;
 		if(ev.keyCode === ENTER_KEYCODE) {
 			var todo = makeTodoList(ev.target.value);
 			var todoList = document.getElementById("todo-list"); 
-			todoList.innerHTML += todo;
+			todoList.insertAdjacentHTML('beforeend', todo);
 			ev.target.value = "";
 		}
 	}
@@ -28,5 +35,16 @@
 		var template = Handlebars.compile(source);
 		var context = {todoTitle: todo};
 		return template(context);
+	}
+
+	function completeTodo(ev) {
+		var input = ev.currentTarget;
+		var li = input.parentNode.parentNode;
+
+		if(input.checked === true) {
+			li.className = "completed";
+		}else {
+			li.className = "";
+		}
 	}
 })();
