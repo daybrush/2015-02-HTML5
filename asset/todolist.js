551276445604a274f9cc2e94812a00ad9d57cb9e
JB <wq1021@naver.com>
2015-07-17 16:50:36 +0900

    complete 서버와 연동되도록 수정

--- asset/todolist.js
+++ asset/todolist.js
@@ -25,13 +25,13 @@ var TODO = {
 		$(window).on("popstate", this.changeURLFilter.bind(this));
 		$(document).on("DOMContentLoaded", function(){
 			$("#new-todo").on("keydown", this.add.bind(this)); 
-			$("#todo-list").on("click", this.completed);
+			$("#todo-list").on("click", this.completed.bind(this));
 			$("#todo-list").on("click", this.remove.bind(this));
 			$("#filters").on("click", this.changeStateFilter.bind(this));
 		}.bind(this));
 		$.get(this.TODO_URL, function(result){
 			for(var i in result){
-				var li = this.build(result[i].todo, result[i].id);
+				var li = this.build(result[i].todo, result[i].id, result[i].completed);
 				$(li[0]).css("opacity", "1");
 				$("#todo-list").append(li[0]);
 			}
@@ -72,8 +72,10 @@ var TODO = {
 		}
 		this.changeSelectedClass(href);
 	},
-	build : function(todo, key){
-		var todoObj = { title : todo , insertId : key}
+	build : function(title, insertId, complete){
+		var completed = (complete === 1)?"completed":"";
+		var checked = (complete === 1)?"checked":"";
+		var todoObj = { title : title , insertId : insertId, completed : completed, checked : checked};
 		var template = Handlebars.compile(Templates.todoTemplate);
 		return $(template(todoObj));
 	},
@@ -81,7 +83,7 @@ var TODO = {
 		if(e.keyCode === this.ENTER_KEYCODE) {
 			var todo = $("#new-todo").val();
 			$.put(this.TODO_URL, {todo : todo}, function(result){
-				var li = this.build(todo, result.insertId);
+				var li = this.build(todo, result.insertId, result.completed);
 				$("#todo-list").prepend(li);
 				$("#new-todo").val("");
 				setTimeout(function(){
@@ -93,8 +95,8 @@ var TODO = {
 	completed : function(e){
 		var input = $(e.target);
 		var li = input.parents("li");
-		var completed = input.is(":checked")?"1":"0";
-		$.post(this.TODO_URL+li.data("key"), function(result){
+		var completed = input.is(":checked")?1:0;
+		$.post(this.TODO_URL+li.data("key"), {completed : completed}, function(result){
 			li.toggleClass("completed", input.is(":checked"));
 		});
 	}, 
@@ -128,9 +130,9 @@ var TODO = {
 
 Templates = {}; 
 Templates.todoTemplate = [
-	'<li data-key={{insertId}} >',
+	'<li data-key={{insertId}} class={{completed}}>',
 		'<div class="view">',
-			'<input class="toggle" type="checkbox">',
+			'<input class="toggle" type="checkbox" {{checked}}>',
 			'<label>{{title}}</label>',
 			'<button class="destroy"></button>',
 		'</div>',
