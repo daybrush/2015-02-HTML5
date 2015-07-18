3373269244f322c0939d118b8c207c9964dd447e
milooy <milooy@icloud.com>
2015-07-19 02:41:34 +0900

    [week3] add 'delete', 'initTODO' with XHR

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,7 +1,4 @@
 /*
-Question
-- domcontentloaded안해도 됨?
-
 TODO
 -완료하기
 삭제하기
@@ -19,8 +16,14 @@ xhr.addEventListener("load", function(e) {
 xhr.send();
 */
 var TODOSync = {
-    get: function() {
-
+    get: function(callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://128.199.76.9:8002/milooy", true); //api
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8"); //헤더설정
+        xhr.addEventListener("load", function(e) {
+            callback(JSON.parse(xhr.responseText)); //JSON.parse하면 객체화된다.
+        });
+        xhr.send();
     },
     add: function(todo, callback) {
         var xhr = new XMLHttpRequest();
@@ -40,21 +43,28 @@ var TODOSync = {
         });
         xhr.send("completed="+param.completed);
     },
-    remove: function() {
-
+    remove: function(param, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("DELETE", "http://128.199.76.9:8002/milooy/"+param.key, true); //api
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8"); //헤더설정
+        xhr.addEventListener("load", function(e) {
+            callback(JSON.parse(xhr.responseText)); //JSON.parse하면 객체화된다.
+        });
+        xhr.send();
     }
 }
 
 var TODO = {
     ENTER_KEYCODE: 13,
     init: function() { //즉시실행함수 삭제
+        this.initTODO();
         $('#new-todo').keydown(this.add.bind(this));
         $('#todo-list').on( "click", '.toggle', this.completed)
         .on( "click", '.destroy', this.remove);
 
     },
     completed: function(e){
-        var completed = $(this).closest('li').hasClass('completed')? '1' : '0';
+        var completed = $(this).closest('li').hasClass('completed')? '0' : '1';
         TODOSync.completed({
             "key": $(this).closest('li').data('key'),
             "completed": completed
@@ -64,14 +74,20 @@ var TODO = {
     },
     remove: function(e) {
         var li = $(this).closest('li');
-        if (!$(this).hasClass('disabled')) {
-            li.css('animation', 'fadeOut .5s');
-            //jQuery 이벤트 훅으로 개선. 이건 두번 다 발생
-            li.on('animationend webkitAnimationEnd',function(){
-                li.remove();
-                $(this).toggleClass('disabled');
-            });
-        }
+
+        TODOSync.remove({
+            "key": $(this).closest('li').data('key'),
+        }, $.proxy(function(){
+            if (!$(this).hasClass('disabled')) {
+                li.css('animation', 'fadeOut .5s');
+                //jQuery 이벤트 훅으로 개선. 이건 두번 다 발생
+                li.on('animationend webkitAnimationEnd',function(){
+                    li.remove();
+                    $(this).toggleClass('disabled');
+                });
+            }
+        }, this));
+
     },
     add: function(e) {
         if($('#new-todo').val() && e.keyCode === this.ENTER_KEYCODE) {
@@ -89,7 +105,28 @@ var TODO = {
                 $("#todo-list").append(template(data));
                 $('#new-todo').val("");
             });
-        } //왜 강의에선 bind this?
+        }
+    },
+    initTODO: function(e) {
+        TODOSync.get(function(json){
+            /* TODO: for를 쓰지 않을 수 있을까?*/
+            for(i in json){
+                var item = json[json.length-i-1]; // item 역순정렬
+                var completed = item.completed==1? 'completed' : null;
+                var source = $("#todo-template").html();
+                var template = Handlebars.compile(source);
+
+                var data = {
+                    todo : item.todo,
+                    key: item.id,
+                    completed: completed
+                };
+
+                $("#todo-list").append(template(data));
+                // 체크박스 속성 checked 추가.
+                if(completed!=null) $("#todo-list li:last-child input").attr("checked", true);
+            }
+        });
     }
 }
 TODO.init();
