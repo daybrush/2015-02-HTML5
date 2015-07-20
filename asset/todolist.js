3ba2b11b73fa0330174baa9c6ae0c28aa5d7b63f
Dahye Kim <dahye.kim@nhnnext.org>
2015-07-20 12:01:15 +0900

    todo delete with animation

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,19 +1,19 @@
 /*todo
 * innerHTML + = todo 로 처리했던 부분 insertAdjacentHTML 로 바꾸기 // Done
-* 등록한 할 일을 완료 처리하기
+* 등록한 할 일을 완료 처리하기 //Done
 *  - 이벤트 할당하기
 *  - class추가하기(li에 completed)
-* 삭제하기
+* 삭제하기 
 *  - 이벤트 할당하기
 *  - li을 서서히 사라지게 처리한 후 삭제
 * 등록하기
 *  - 애니메이션 기능을 추가
-
 */
 
 $(document).ready(function () {
 	$("#new-todo").on("keydown", addTodo);
 	$("#todo-list").on("click", "input", completeTodo);
+	$("#todo-list").on("click", "button", deleteTodo);
 
 	function addTodo(ev) {
 		var ENTER_KEYCODE = 13;
@@ -41,9 +41,25 @@ $(document).ready(function () {
 			li.className = "";
 		}
 	}
-});
 
-(function () {
-	//이벤트 등록
+	function deleteTodo(ev) {
+		var button = ev.currentTarget;
+		var li = button.parentNode.parentNode;
+		var start = null;
+		window.requestAnimationFrame(deleteStep); //질문. requestAnimationFrame은 window에만 바인딩해줘야 하는걸까? li에 바인딩 해주면 안되나?
+		
+		function deleteStep(timestamp) {
+			if(!start) start = timestamp;
+				var progress = timestamp - start;
+		  		li.style.opacity = 1 - progress * 0.01;
+		 	if (progress < 200) {
+		    	window.requestAnimationFrame(deleteStep);
+		 	}
+		  	else {
+		  		li.parentNode.removeChild(li);
+		  	}
+		}
+	}
+
 	
-})();
+});
