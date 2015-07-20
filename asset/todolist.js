e4b6c868fba6e5ee8f10e2a147149d6c21a3d904
byjo <buyongjo@gmail.com>
2015-07-20 14:37:29 +0900

    [week03] add, initData의 중복 로직 리팩토링

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,11 +1,13 @@
 // TODO 
-// TODO : initData, add 중복 제거
 // jquery의 select 사용시 [0] 이런식으로밖에 못쓰ㄴ ㅏ....
 // TODOsync : add, complete 등 data를 전달하는 ajax통신에서 fetch 사용
 
 var TODO = {
 	ENTER_KEYCODE : 13,
 
+	/*
+	 * init : variable 선언, event bind
+	 */
 	init : function() {
 		document.addEventListener('DOMContentLoaded', function(){
 
@@ -25,28 +27,23 @@ var TODO = {
 		}.bind(this));
 	},
 
-	//TODO : add일 때와 중복되는 로직 리팩토링
+	/*
+	 * initData : 서버에 저장된 데이터 불러오기
+	 */
 	initData : function() {
 		TODOsync.get(function(json){
-			// for in으로 접근하면 string이 되는 이유는?
-			// for (var todo in json) {
-			// 	console.log(todo);
-			// 	console.log(todo.todo);
-			// }
 			json.forEach(function(element) {
 				var context = {todo: element.todo};
 				this.todo = $(this.todoTemplate(context));
 				this.todo[0].dataset.key = element.id;
+
 				if (element.completed) {
 					this.todo[0].querySelector('.toggle').checked = element.completed;
 					this.todo.addClass('completed');
 				}
 
-				this.ulTodoList.append(this.todo);
-				this.appendingAnimate.bind(this)();
-				
-				$('#todo-list li:last').on('click', '.toggle', this.complete);
-				$('#todo-list li:last').on('click', '.destroy', this.remove);
+				this.append.bind(this)();
+
 			}.bind(this));
 
 		}.bind(this));
@@ -56,25 +53,29 @@ var TODO = {
 		if (e.keyCode === this.ENTER_KEYCODE) {
 			var context = {todo: this.inputTodo[0].value};
 			TODOsync.add(context.todo, function(json){
-				this.inputTodo[0].value = "";
+				this.inputTodo[0].value = '';
 
 				this.todo = $(this.todoTemplate(context));
 				this.todo[0].dataset.key = json.insertId;
-				
 				//왜 안됨
 				//this.todo.data('key', json.insertId);
 
-				this.ulTodoList.append(this.todo);
-				
+				this.append.bind(this)();
 
-				this.appendingAnimate.bind(this)();
-				
-				$('#todo-list li:last').on('click', '.toggle', this.complete);
-				$('#todo-list li:last').on('click', '.destroy', this.remove);
-				
 			}.bind(this));
-
 		}
+	},
+
+	complete : function(e) {
+		$targetLi = $(e.delegateTarget);
+		var completed = $targetLi.find('.toggle')[0].checked?'1':'0';
+
+		TODOsync.complete({
+			'key' : $targetLi.data('key'),
+			'completed' : completed 
+		}, function() {
+			$targetLi.toggleClass('completed');
+		});
 	},
 
 	// js에선 function의 parameter에 상관없이 함수 이름으로만 정의를 하나?
@@ -86,18 +87,15 @@ var TODO = {
 		});
 	},
 
-	complete : function(e) {
-		$targetLi = $(e.delegateTarget);
-		// var completed = $targetLi.hasClass('completed')?'1':'0';
-		var completed = $targetLi.find('.toggle')[0].checked?'1':'0';
-
-		TODOsync.complete({
-			'key' : $targetLi.data('key'),
-			//'completed' : $targetLi.hasClass('completed')?'1':'0'; 
-			'completed' : completed 
-		}, function() {
-			$targetLi.toggleClass('completed');
-		});
+	/*
+	 * append : li로 된 todo DOM에 append
+	 */
+	append : function() {
+		this.ulTodoList.append(this.todo);
+		this.appendingAnimate.bind(this)();
+		
+		$('#todo-list li:last').on('click', '.toggle', this.complete);
+		$('#todo-list li:last').on('click', '.destroy', this.remove);
 	},
 
 	appendingAnimate : function() {
@@ -106,7 +104,6 @@ var TODO = {
 		$('.appending').removeClass('appending');
 	}
 }
-TODO.init();
 
 var myHeaders = new Headers();
 myHeaders.append('Content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
@@ -131,9 +128,10 @@ var TODOsync = {
 			callback(response);
 		});
 		
+		// 안돼애애애애애 ㅠㅠㅠㅠㅠㅠ
 		// var form = new FormData();
 		// form.append('todo', todo);
-		// fetch(this.URL, {method : 'PUT', headers : myHeaders, body : form}).then(function(response) {
+		// fetch(this.URL, {method : 'PUT', headers : myHeaders, data : {'todo' : todo}}).then(function(response) {
 		// 	return response.json();
 		// }).then(function(response) {
 		// 	callback(response);
@@ -161,3 +159,5 @@ var TODOsync = {
 
 	}
 }
+
+TODO.init();
