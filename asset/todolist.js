e661274c4b844a8570a5ad42256ceb77d53b14f8
helloheesu <heesu@nhnnext.org>
2015-07-21 17:14:17 +0900

    refactor a little
    - https://github.com/helloheesu/2015-02-HTML5/commit/0d72aeff75feb2382773dc65e9684db3d3720a0d#diff-1d96423cf8afbaa86b544c085b081a4aR69
    - https://github.com/helloheesu/2015-02-HTML5/commit/4ce1717b65e4aced4a58b969462dac22b40813f9#commitcomment-12266770
    - https://github.com/helloheesu/2015-02-HTML5/commit/4ce1717b65e4aced4a58b969462dac22b40813f9#commitcomment-12266817 (camel 보다 hyphen 을 많이들 쓰는 것 같다)

--- asset/todolist.js
+++ asset/todolist.js
@@ -49,88 +49,85 @@ TODO = {
   item : null,
   board : null,
   template : function(){},
-};
-TODO.init = function () {
-  TODO.item = $('#new-item-script');
-  TODO.board = $('#todo-list');
-  TODO.template = Handlebars.compile(TODO.item.html());
+  init : function () {
+    TODO.item = $('#new-item-script');
+    TODO.board = $('#todo-list');
+    TODO.template = Handlebars.compile(TODO.item.html());
 
-  TODO.get();
-};
-TODO.addItem = function (sContents) {
-  TODOsync.add(sContents, function (data) {
-    var elNewItem = $(TODO.template({'item':{
-      'todo':sContents,
-      'id':data.insertId
-    }}));
-    TODO.board.append(elNewItem);
-    elNewItem.css('opacity', 0);
-    elNewItem.css('opacity');
-    elNewItem.css('opacity', 1);
-  });
-};
-TODO.get = function () {
-  TODOsync.get(function (data) {
-    data.sort(function (a, b) {
-      if(a.id < b.id)
-        return -1;
-      else if(a.id > b.id)
-        return 1;
-      else
-        return 0;
+    TODO.bindEvent();
+    TODO.get();
+  },
+  add : function (sContents) {
+    TODOsync.add(sContents, function (data) {
+      var elNewItem = $(TODO.template({'item':{
+        'todo':sContents,
+        'id':data.insertId
+      }}));
+      TODO.board.append(elNewItem);
+      elNewItem.css('opacity', 0);
+      elNewItem.css('opacity');
+      elNewItem.css('opacity', 1);
     });
-    TODO.board.append(TODO.template({item:data}));
-  })
-};
-TODO.removeItem = function ($cachedLi) {
-  if (!$cachedLi) return;
-  var itemId = $cachedLi.data('id');
+  },
+  get : function () {
+    TODOsync.get(function (data) {
+      data.sort(function (a, b) {
+        if(a.id < b.id) {return -1;}
+        else if(a.id > b.id) {return 1;}
+        else {return 0;}
+      });
+      TODO.board.append(TODO.template({item:data}));
+    })
+  },
+  remove : function ($cachedLi) {
+    if (!$cachedLi) {return;}
+    var itemId = $cachedLi.data('id');
 
-  TODOsync.remove(itemId, function () {
-    $cachedLi.css({
-      'opacity': 0,
-      'maxHeight': 0,
-      'overflow': 'hidden'
+    TODOsync.remove(itemId, function () {
+      $cachedLi.css({
+        'opacity': 0,
+        'max-height': 0,
+        'overflow': 'hidden'
+      });
+      var remove = function (event) {
+        if (event.eventPhase !== 2) {
+          return;
+        }
+        $cachedLi.remove();
+      };
+      $cachedLi.on('webkitTransitionEnd transitionend', remove);
     });
-    var removeItem = function (event) {
-      if (event.eventPhase !== 2) {
-        // li 자신이 아니라 button 등 자식들에 의해 bubbling 받은 거면 무시.
-        ////////// 더 좋은방식은 없나 애초에 자기자신 이벤트만 받는거 같은?
-        return;
-      }
-      $cachedLi.off('webkitTransitionEnd transitionend', removeItem);
-      $cachedLi.remove();
-    };
-    $cachedLi.on('webkitTransitionEnd transitionend', removeItem);
-  });
-};
-TODO.completeItem = function ($item, $event) {
-  var itemId = $item.closest('li').data('id');
-  var checked = $item.is(':checked');
-  
-  TODOsync.complete(itemId, checked, function () {
-    $item.prop("checked", checked);
-    $item.closest('li').toggleClass('completed', checked);
-  });
+  },
+  complete : function ($item, $event) {
+    var itemId = $item.closest('li').data('id');
+    var checked = $item.is(':checked');
+    
+    TODOsync.complete(itemId, checked, function () {
+      $item.prop("checked", checked);
+      $item.closest('li').toggleClass('completed', checked);
+    });
 
-  $event.preventDefault();
+    $event.preventDefault();
+  },
+  bindEvent : function () {
+    $('#new-todo').on('keypress', function(e) {
+      var ENTER_KEYCODE = 13;
+      if(e.which !== ENTER_KEYCODE) {return;}
+      var sContents = $('#new-todo').val();
+      if(!sContents) {return;}
+      TODO.add(sContents);
+      $('#new-todo').val('');
+    });
+    $('#todo-list').on('click', 'input.toggle', function(e) {
+      TODO.complete($(this), e);
+    });
+    $('#todo-list').on('click', 'li:not(.deleting) button.destroy', function() {
+      TODO.remove($(this).closest('li'));
+    });
+  }
 };
+
 
 $(document).ready(function () {
   TODO.init();
-  $('#new-todo').on('keypress', function(event) {
-    var ENTER_KEYCODE = 13;
-    if(event.which === ENTER_KEYCODE) {
-      var sContents = $('#new-todo').val();
-      if(!sContents) return;
-      TODO.addItem(sContents);
-      $('#new-todo').val('');
-    }
-  });
-  $('#todo-list').on('click', 'input.toggle', function(e) {
-    TODO.completeItem($(this), e);
-  });
-  $('#todo-list').on('click', 'li:not(.deleting) button.destroy', function() {
-    TODO.removeItem($(this).closest('li'));
-  });
 });
