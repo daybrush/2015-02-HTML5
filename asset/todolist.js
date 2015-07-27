26cd39bc251e3ea613f65cdcfebf5efb16ccaebe
GraceLilac <lovelove8926@naver.com>
2015-07-28 04:54:03 +0900

    mission 4

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,8 +1,28 @@
 var TODOSync = {
+	url : "http://128.199.76.9:8002/",
+	id : "kimhyewon",
+	init : function() {
+		window.addEventListener("online", this.onofflineListener);	//온라인일 때 발생
+		window.addEventListener("offline", this.onofflineListener);	//오프라인일 때 발생 
+	},
+	onofflineListener : function() {
+		// if(navigator.onLine) { 	//온라인이면 
+		// 	document.getElementById("header").classList.remove("offline");
+		// } else {
+		// 	document.getElementById("header").classList.add("offline");
+		// }
+
+		document.getElementById("header").classList[navigator.onLine?"remove":"add"]("offline");
+		//윗 부분과 같은 코드다. 삼항연산자 	
+
+		if(navigator.onLine) {
+			//서버로 싱크 맞추기 
+		}
+	},
 	get : function(callback) {
 		var xhr = new XMLHttpRequest();
 		
-		xhr.open("GET", "http://128.199.76.9:8002/kimhyewon/", true);
+		xhr.open("GET", this.url+this.id, true);
 		
 		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
 		
@@ -13,22 +33,26 @@ var TODOSync = {
 		xhr.send(null);
 	},
 	add : function(todo, callback) {
-		var xhr = new XMLHttpRequest();
+		if(navigator.onLine) {
+			var xhr = new XMLHttpRequest();
 		
-		xhr.open("PUT", "http://128.199.76.9:8002/kimhyewon/", true);
+			xhr.open("PUT", this.url+this.id+"/", true);
 		
-		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+			xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
 		
-		xhr.addEventListener("load", function(e) {
-			callback(JSON.parse(xhr.responseText))
-		});
+			xhr.addEventListener("load", function(e) {
+				callback(JSON.parse(xhr.responseText))
+			});
 		
-		xhr.send("todo=" + todo);
+			xhr.send("todo=" + todo);
+		} else {
+			//data를 클라이언트에 저장. localStorage(많이사용), indexedDB, websql
+		}
 	},
 	completed : function(param, callback) {
 		var xhr = new XMLHttpRequest();
 		
-		xhr.open("POST", "http://128.199.76.9:8002/kimhyewon/" + param.key, true);
+		xhr.open("POST", this.url+this.id+"/" + param.key, true);
 		
 		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
 		
@@ -41,7 +65,7 @@ var TODOSync = {
 	deleted : function(param, callback) {
 		var xhr = new XMLHttpRequest();
 		
-		xhr.open("DELETE", "http://128.199.76.9:8002/kimhyewon/" + param.key, true);
+		xhr.open("DELETE", this.url+this.id+"/" + param.key, true);
 		
 		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
 		
@@ -55,14 +79,77 @@ var TODOSync = {
 
 var TODO = {
 	ENTER_KEYCODE : 13,
+	selectedIndex : 0,
 	init : function() {
 		document.addEventListener("DOMContentLoaded", function() {
 			document.getElementById("new-todo").addEventListener("keydown", this.add.bind(this));
 			document.getElementById("todo-list").addEventListener("click", this.completed); //this 안썻기 때메 굳이 bind 필요 없다 
 			document.getElementById("todo-list").addEventListener("click", this.deleted);
 			this.get();
+			//밑의 필터 클릭시 클릭된 엘리먼트에 효과주는 이벤트 
+			document.getElementById("filters").addEventListener("click", this.changeStateFilter.bind(this));
+			window.addEventListener("popstate", this.changeURLFilter.bind(this));
+			
 		}.bind(this))
 	},
+	changeURLFilter : function(e) { 	//뒤로가기 관련 
+		if(e.state) {	//있는 경우 
+			var method = e.state.method;
+			
+			// if(method === "all") {	//해당 뷰를 호출 
+			// 	this.allView();
+			// } else if(method === "active") {
+			// 	this.activeView();
+			// } else if(method === "completed") {
+			// 	this.completedView();
+			// } 
+
+			this[method+"View"]();	//위 코드와 같다. 이렇게 오브젝트 리터럴을 호출할 수 있다. 많이 사용됨.
+
+		} else {
+			this.allView(); //첫번째 페이지니까 이걸 호출 
+		}
+	},
+	changeStateFilter : function(e) {
+		var target = e.target;
+		var tagName = target.tagName.toLowerCase(); //대소문자 처리 
+		if(tagName == "a") {
+			var href = target.getAttribute("href");
+			if(href === "index.html") {
+				this.allView();
+				history.pushState({"method":"all"},null,"index.html"); //파라미터, 타이틀명, 페이지명 
+			} else if(href === "active") {
+				this.activeView();
+				history.pushState({"method":"active"},null,"active");
+			} else if(href === "completed") {
+				this.completedView();
+				history.pushState({"method":"completed"},null,"completed");
+			}
+		}
+		e.preventDefault(); //a 태그라서 페이지 이동하므로 막아줌 
+	},
+	allView : function() { 	//전체보기 
+		document.getElementById("todo-list").className = "";
+		this.selectedNavigator(0);
+		// history.pushState({"method":"all"},null,"index.html"); //위로 옮겨야 한다. 여기 있으면 다시 추가되기 때문에. 
+	},
+	activeView : function() {
+		document.getElementById("todo-list").className = "all-active";
+		this.selectedNavigator(1);
+	},
+	completedView : function() {
+		document.getElementById("todo-list").className = "all-completed";
+		this.selectedNavigator(2);
+	},
+	selectedNavigator : function(index) {
+		var navigatorList = document.querySelectorAll("#filters a"); //All인 이유는 리스트 다 받아와야 해서.
+		navigatorList[this.selectedIndex].classList.remove("selected"); //기존에 선택된 건 삭제하고,
+		navigatorList[index].classList.add("selected"); // 새로운 엘리먼트 인덱스는 추가. 
+		this.selectedIndex = index; //다시 담기 
+	},
+	// changeState : function(index){
+
+	// },
 	get : function() {
 		TODOSync.get(function(e){
 			e = e.sort(function(a,b){
@@ -138,7 +225,6 @@ var TODO = {
 		var button = e.target;
 		if(button.className === "destroy") {	//이 부분 꼭 필요! 
 			var li = e.target.parentNode.parentNode;
-			console.log(li);
 
 			TODOSync.deleted({
 				"key" : li.dataset.key
@@ -154,5 +240,5 @@ var TODO = {
 };
 
 TODO.init();
-
+TODOSync.init();
 
