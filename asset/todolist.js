14d6b314f92f3a70c04aac400ce91931f9b5a321
Dahye Kim <dahye.kim@nhnnext.org>
2015-07-31 09:05:10 +0900

    ajax request for todo complete

--- asset/todolist.js
+++ asset/todolist.js
@@ -20,7 +20,8 @@ var TODOSync = {
 	},
 
 	complete: function(oParam, callback) {
-		var param = { method: "POST", url: this.makeUrl("/"+oParam.todoKey), data: "complete=" + oParam.complete, success}
+		var param = { method: "POST", url: this.makeUrl("/"+oParam.todoKey), data: "completed=" + oParam.complete };
+		$.ajax(param).then(callback, this.alertAjaxFail);
 	},
 
 	remove: function() {},
@@ -47,7 +48,8 @@ var TODO = {
 	get: function() {
 		TODOSync.get(function (json) {
 			$.each(json, function(i, item) {
-			    var todoList = this.makeTodoList(item.todo);
+				var completed =  item.completed===1? true : false;
+			    var todoList = this.makeTodoList({key: item.id, title: item.todo, completed: completed}); // "id", "todo", "completed"는 api 상수니까 나중에 따로 뺴서 정리하자
 			    $(todoList).prependTo("#todo-list");
 			}.bind(this));
 		}.bind(this));
@@ -59,28 +61,34 @@ var TODO = {
 			var sTodo = ev.target.value;
 			
 			TODOSync.add(sTodo, function(json) {
-				var todoList = this.makeTodoList(sTodo);
+				var todoList = this.makeTodoList({key: json.id, title: sTodo});
 				$("#todo-list").append(todoList);
 				$("#new-todo").val("");
 			}.bind(this));
 		}
 	},
 
-	makeTodoList : function (sTodo) {
+	makeTodoList : function (oTodo) {
 		var source = $("#todo-template").html();
 		var template = Handlebars.compile(source);
-		var context = {todoTitle: sTodo};
+		var context = { completed:oTodo.completed, todoKey: oTodo.key, todoTitle: oTodo.title };
 		return template(context);
 	},
 
 	complete : function (ev) {
 		var input = ev.currentTarget;
-		var li = input.parentNode.parentNode;
-		if(input.checked === true) {
-			li.className = "completed";
-		}else {
-			li.className = "";
+		var li = input.parentNode;
+		var oParam = {
+			todoKey: li.dataset.todoKey,
+			complete: input.checked? 1 : 0
 		}
+		TODOSync.complete(oParam, function(json){
+			if(oParam.complete === 1) {
+				li.className = "completed";
+			}else {
+				li.className = "";
+			}
+		})
 	},
 
 	startDeleteAnimation : function (ev) {
