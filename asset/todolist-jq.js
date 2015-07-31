e7d6d45ff22e41a7c83c6b84561aaee703102321
dragonist <erin314@naver.com>
2015-07-31 20:33:07 +0900

    week4 구현 완

--- asset/todolist-jq.js
+++ asset/todolist-jq.js
@@ -1,86 +1,160 @@
-var TODOSync = {
+var TODOOnline = {
     URL : "http://128.199.76.9:8002/nickname",
-    init : function () {
-        window.addEventListener("online", this.onoffLineListener);
-        window.addEventListener("offline", this.onoffLineListener);
+    get : function(callback) {
+        $.ajax({method: "GET", url: this.URL}).done(callback);
     },
-    onoffLineListener : function () {
-        console.log("hello");
-        document.getElementById('header').classList[navigator.onLine?"remove":"add"]("offline");
-        if(navigator.onLine){
-            //서버로 Sync 맞추기
+    add : function(todo,callback) {
+        $.ajax({method: "PUT", url: this.URL, data: "todo="+todo}).done(callback);
+    },
+    completed : function(param,callback) {
+        $.ajax({method: "POST", url: this.URL+'/'+param.key, data:"completed="+param.completed}).done(callback);
+    },
+    remove : function(param,callback) {
+        $.ajax({method: "DELETE", url: this.URL+'/'+param.key}).done(callback);
+    },
+    sync : function () {
+        // from offline to line
+        $.each(localStorage, function (key, value) {
+            var item = JSON.parse(value);
+            if(item.method){
+                if(item.method === "add"){
+                    TODOOnline.add(item.todo, function (json) {
+                        if(item.completed){
+                            item.key = json.insertId;
+                            TODOOnline.completed(item);
+                        }
+                    });
+                }else{
+                    TODOOnline[item.method](item);
+                }
+            }
+        });
+        localStorage.clear();
+        // 위에 비동기로 처리되고 있는 로직이 완료 되면 refresh 가 하고 싶어요
+        var temp = setTimeout(function () {
+            location.reload();
+        }, 1000);
+    }
+}
+
+var TODOOffline = {
+    count : 0,
+    check: function () {
+        if(!Storage){
+            alert("sorry, your computer is not supported db");
         }
     },
     get : function(callback) {
-        if (navigator.onLine) {
-            $.ajax({method: "GET", url: this.URL}).done(callback);
-        }else{
-            //local db
-        }
+        // console.log(there is off line);
     },
     add : function(todo,callback) {
-        if (navigator.onLine) {
-            $.ajax({method: "PUT", url: this.URL, data: "todo="+todo}).done(callback);
-        };
+        TODOOffline.check();
+        var item = {"method": "add", "todo": todo, "key": TODOOffline.count,"only-local": true};
+        localStorage.setItem(item.key, JSON.stringify(item));
+        callback({insertId:item.key});
+        TODOOffline.count++;
     },
     completed : function(param,callback) {
-        if (navigator.onLine) {
-            $.ajax({method: "POST", url: this.URL+'/'+param.key, data:"completed="+param.completed}).done(callback);
-        };
+        TODOOffline.check();
+        var item = JSON.parse(localStorage.getItem(param.key));
+        if(!item.method){
+            item.method = "completed";            
+        }
+        item.completed = param.completed;
+        localStorage.setItem(param.key, JSON.stringify(item));
+        callback();
     },
     remove : function(param,callback) {
-        if (navigator.onLine) {
-            $.ajax({method: "DELETE", url: this.URL+'/'+param.key}).done(callback);
-        };
+        TODOOffline.check();
+        var item = JSON.parse(localStorage.getItem(param.key));
+        if(item["only-local"]){
+            localStorage.removeItem(param.key);
+        }else{
+            item.method = "remove";
+            localStorage.setItem(param.key, JSON.stringify(item));
+        }
+        callback();
+        // debugger;
+    },
+    sync : function () {
+        // from online to offline
+        var list = $("#todo-list li");
+        $.each(list, function (index, value) {
+            var key = value.dataset.key;
+            localStorage.setItem(key, JSON.stringify({"method": null, "key": key}));
+        })
+    }
+}
+
+var TODOSync = {
+    DataManager : null,
+    init : function () {
+        window.addEventListener("online", this.onoffLineListener.bind(this));
+        window.addEventListener("offline", this.onoffLineListener.bind(this));
+        document.getElementById('header').classList[navigator.onLine?"remove":"add"]("offline");
+        this.DataManager = navigator.onLine? TODOOnline : TODOOffline;
+
+    },
+    onoffLineListener : function () {
+        document.getElementById('header').classList[navigator.onLine?"remove":"add"]("offline");
+        this.DataManager = navigator.onLine? TODOOnline : TODOOffline;
+        this.DataManager.sync();
     }
 }
 
 var TODO = {
-    oInput: $("#new-todo"),
-    oList : $("#todo-list"),
+    oInput: null,
+    oList : null,
     ENTER_KEYCODE : 13,
-    ShowState : {index:0, className:""},
+    ShowState : "index.html",
+    States : {
+        "index.html" : {index: 0, name:""},
+        "active" : {index: 1, name:"all-active"},
+        "completed" : {index: 2, name:"all-completed"}
+    },
     init: function () {
+        this.oInput = $("#new-todo").eq(0);
+        this.oList = $("#todo-list").eq(0);
+
         this.oInput.on("keydown", this.add.bind(this));
-        $("#todo-list").on("click", ".toggle", this.completed);
-        $("#todo-list").on("click", ".destroy", this.remove);
+        this.oList.on("click", ".toggle", this.completed);
+        this.oList.on("click", ".destroy", this.remove);
         this.get();
         $("#filters").on("click", "a", this.changeStateFilter);
-        window.addEventListener("popState", this.changeUrlFilter);
+        window.addEventListener("popstate", this.changeUrlFilter.bind(this));
     },
-    changeUrlFilter : function () {
-        
-    }
-    changeStateFilter : function (e) {
-        var href = this.getAttribute("href");
-        // console.log(href)
-        e.preventDefault();
-        if(href === "index.html"){
-            TODO.selectView({
-                index:0, className:"", 
-                method: "all", url: href});
-        }else if(href === "active"){
-            TODO.selectView({
-                index:1, className:"all-active", 
-                method:"active", url: href});
-        }else if(href === "completed"){
-            TODO.selectView({
-                index:2, className:"all-completed", 
-                method:"completed", url: href});
+    changeUrlFilter : function (e) {
+        if (e.state) {
+            var href = e.state.href;
+            this.selectView(href);
+        }else{
+            this.selectView("index.html");
         }
     },
-    selectView : function (nowState) {
+    changeStateFilter : function (e) {
+        var href = this.getAttribute("href");
+        TODO.selectView(href, TODO.saveState);
+        // callback 동작 않됌 ㅠㅠ
+        // TODO.selectView(href, history.pushState);
+        e.preventDefault();
+    },
+    selectView : function (nowState, callback) {
         var navigatorList = $("#filters a");
-        $(navigatorList.get(this.ShowState.index)).removeClass("selected");
-        $("#todo-list").removeClass(this.ShowState.className);
+        var recent = this.States[this.ShowState];
+        var newone = this.States[nowState];
 
-        $(navigatorList.get(nowState.index)).addClass("selected");
-        $("#todo-list").addClass(nowState.className);
-        this.ShowState = nowState;
-        // 히스토리 추가
-        // 뒤로가기 이벤트 변경
-        console.log(nowState);
-        history.pushState(nowState.method, null, nowState.href);
+        $(navigatorList.get(recent.index)).removeClass("selected");
+        $(navigatorList.get(newone.index)).addClass("selected");
+        this.oList.removeClass(recent.name);
+        this.oList.addClass(newone.name);
+        TODO.ShowState = nowState;
+
+        if(callback){
+            callback(nowState);
+        }
+    },
+    saveState : function (nowState) {
+        history.pushState({"href": nowState}, null, null);
     },
     selectedNavigator : function (index) {
         var navigatorList = $("#filters a");
@@ -93,35 +167,30 @@ var TODO = {
     	this.build = template;
         return template(context);
     },
-    get : function () { 
-        TODOSync.get(function (json) {
+    get : function () {
+        TODOSync.DataManager.get(function (json) {
             $("#todo-list").append(json.map(function (obj) {
                 return TODO.build({target: obj.todo, key: obj.id, completed: obj.completed});
             }).join(''));
-            setTimeout(function () {
-                $("li").removeClass("appending");
-            }, 0);
+            $("li").get(0).offsetHeight;
+            $("li").removeClass("appending");
         })
     },
     completed : function (e) {
         var li = $(e.currentTarget).closest("li");
         var checked = e.currentTarget.checked?"1":"0";
 
-        TODOSync.completed({
+        TODOSync.DataManager.completed({
             "key" : li.data("key"),
             "completed" : checked
         }, function () {
-            if(e.currentTarget.checked){
-                li.addClass("completed");
-            }else{
-                li.removeClass("completed");
-            }
-        })
+            li.toggleClass("completed");
+        });
     },
     remove : function (e) {
         var li = $(e.currentTarget).closest("li")
         li.one("transitionend", function () {
-            TODOSync.remove({
+            TODOSync.DataManager.remove({
                 "key": li.data("key")
             }, function () {
                 li.remove();
@@ -131,22 +200,18 @@ var TODO = {
     },
     add : function (e) {
         if (e.keyCode !== this.ENTER_KEYCODE) {return}
-        var todo = TODO.oInput.val();
-        TODOSync.add(todo, function (json) {
+        var todo = this.oInput.val();
+        TODOSync.DataManager.add(todo, function (json) {
             $("#todo-list").prepend(TODO.build({target: todo, key: json.insertId}));
-            setTimeout(function () {
-                $("li").removeClass("appending");
-            }, 0);
-            // 왜 에가 나는지 모르겠어요!!
-            // Invalid left-hand side in assignment
-            // TODO.oInput.val() = ""; 
+            $("li").get(0).offsetHeight;
+            $("li").removeClass("appending");
             TODO.oInput.val('')
         });
     }
 }
 
 $(function () {
-    TODO.init();
     TODOSync.init();
+    TODO.init();
 });
 
--- asset/todolist.js
+++ asset/todolist.js
@@ -1,62 +1,183 @@
-//document.addEventListener["load", fp];
-//document.addEventListener["DOMContentLoaded", fp];
-
-
-var TODOSync = {
+var TODOOnline = {
+    URL : "http://128.199.76.9:8002/nickname",
+    xhrManager:function (param, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(param.reqMethod, param.url, true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+        if(callback){
+            xhr.addEventListener("load", function (e) {
+                callback(JSON.parse(xhr.responseText));
+            });
+        }
+        xhr.send(param.reqBody);
+    },
     get : function (callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://128.199.76.9:8002/nickname", true);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
-        xhr.addEventListener("load", function (e) {
-            callback(JSON.parse(xhr.responseText));
-        });
-        xhr.send();
+        var param = {reqMethod: "GET", url: TODOOnline.URL};
+        TODOOnline.xhrManager(param, callback);
     },
-    add : function (todo, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("PUT", "http://128.199.76.9:8002/nickname", true);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
-        xhr.addEventListener("load", function (e) {
-            callback(JSON.parse(xhr.responseText));
-        });
-        xhr.send("todo="+todo);
+    add : function (data, callback) {
+        var param = {reqMethod: "PUT", url: TODOOnline.URL, reqBody : "todo="+data.todo};
+        TODOOnline.xhrManager(param, callback);
     },
-    completed : function (param, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "http://128.199.76.9:8002/nickname/"+param.key, true);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
-        xhr.addEventListener("load", function (e) {
-            callback(JSON.parse(xhr.responseText));
-        });
-        xhr.send("completed="+param.completed);
+    completed : function (data, callback) {
+        var param = {reqMethod: "POST", url : TODOOnline.URL+"/"+data.key, reqBody : "completed="+data.completed};
+        TODOOnline.xhrManager(param, callback);
     },
-    remove : function(param, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("DELETE", "http://128.199.76.9:8002/nickname/"+param.key, true);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded;charset=UTF-8");
-        xhr.addEventListener("load", function (e) {
-            callback(JSON.parse(xhr.responseText));
+    remove : function(data, callback) {
+        var param = {reqMethod: "DELETE", url : TODOOnline.URL+"/"+data.key};
+        TODOOnline.xhrManager(param, callback);
+    },
+    sync : function () {
+        // from offline to line
+        $.each(localStorage, function (key, value) {
+            var item = JSON.parse(value);
+            if(item.method){
+                if(item.method === "add"){
+                    debugger;
+                    TODOOnline.add({"todo":item.todo}, function (json) {
+                        if(item.completed){
+                            item.key = json.insertId;
+                            TODOOnline.completed(item);
+                            debugger;
+                        }
+                    });
+                }else{
+                    TODOOnline[item.method](item);
+                }
+            }
         });
-        xhr.send();
+        localStorage.clear();
     }
 }
 
+var TODOOffline = {
+    count : 0,
+    check: function () {
+        if(!Storage){
+            alert("sorry, your computer is not supported db");
+        }
+    },
+    get : function(callback) {
+        // console.log(there is off line);
+    },
+    add : function(data,callback) {
+        TODOOffline.check();
+        var item = {"method": "add", "todo": data.todo, "key": TODOOffline.count,"only-local": true};
+        localStorage.setItem(item.key, JSON.stringify(item));
+        callback({insertId:item.key});
+        TODOOffline.count++;
+    },
+    completed : function(data,callback) {
+        TODOOffline.check();
+        var item = JSON.parse(localStorage.getItem(data.key));
+        if(!item.method){
+            item.method = "completed";            
+        }
+        item.completed = data.completed;
+        debugger;
+        localStorage.setItem(data.key, JSON.stringify(item));
+        callback();
+    },
+    remove : function(data,callback) {
+        TODOOffline.check();
+        var item = JSON.parse(localStorage.getItem(data.key));
+        if(item["only-local"]){
+            localStorage.removeItem(data.key);
+        }else{
+            item.method = "remove";
+            localStorage.setItem(data.key, JSON.stringify(item));
+        }
+        callback();
+        // debugger;
+    },
+    sync : function () {
+        // from online to offline
+        var list = $("#todo-list li");
+        $.each(list, function (index, value) {
+            var key = value.dataset.key;
+            debugger;
+            localStorage.setItem(key, JSON.stringify({"method": null, "key": key}));
+        })
+    }
+}
+
+
+
 var TODO = {
     ENTER_KEYCODE : 13,
+    oInput : null,
+    oList : null,
+    oHeader : null,
+    oFilter : null,
+    DataManager : null,
+    ShowState : "index.html",
+    States : {
+        "index.html" : {index: 0, name: null},
+        "active" : {index: 1, name:"all-active"},
+        "completed" : {index: 2, name:"all-completed"}
+    },
     init: function () {
         document.addEventListener("DOMContentLoaded", function() {
-            this.target = document.getElementById("new-todo");
-            document.getElementById("new-todo").addEventListener("keydown",this.add.bind(this));
+            this.oInput = document.getElementById("new-todo");
+            this.oList = document.getElementById("todo-list");
+            this.oHeader = document.getElementById("header");
+            this.oFilter = document.getElementById("filters");
+
+            this.oInput.addEventListener("keydown",this.add.bind(this));
+            this.oList.addEventListener("click", this.completed.bind(this));
+            this.oList.addEventListener("click", this.remove.bind(this));
+
+            this.oFilter.addEventListener("click", this.changeStateFilter.bind(this));
+            window.addEventListener("popstate", this.changeUrlFilter.bind(this));
+
+            window.addEventListener("online", this.onoffLineListener.bind(this));
+            window.addEventListener("offline", this.onoffLineListener.bind(this));
+            this.oHeader.classList[navigator.onLine?"remove":"add"]("offline");
+            this.DataManager = navigator.onLine? TODOOnline : TODOOffline;
+
+
             this.get();
-            // $("#todo-list").on("click", ".toggle", this.completed.bind(this));
-            $("#todo-list").on("click", ".toggle", function () {
-                TODO.completed(this);
-            });
-            
-            $("#todo-list").on("click", ".destroy", function () {
-                TODO.remove(this);
-            });
         }.bind(this));
+    },
+    changeStateFilter : function (e) {
+        if(e.target.tagName === 'A'){
+            var href = e.target.getAttribute("href");
+            this.selectView(href, this.saveState);
+            e.preventDefault();
+        }
+    },
+    selectView : function (nowState, callback) {
+        var navigatorList = this.oFilter.getElementsByTagName('A');
+        var recent = this.States[this.ShowState];
+        var newone = this.States[nowState];
+
+        navigatorList[recent.index].classList.remove("selected");
+        navigatorList[newone.index].classList.add("selected");
+        this.oList.classList.remove(recent.name);
+        this.oList.classList.add(newone.name);
+        
+        TODO.ShowState = nowState;
+
+        if(callback){
+            callback(nowState);
+        }
+    },
+    saveState : function (nowState) {
+        history.pushState({"href": nowState}, null, null);
+    },
+    changeUrlFilter : function (e) {
+        if (e.state) {
+            var href = e.state.href;
+            this.selectView(href);
+        }else{
+            this.selectView("index.html");
+        }
+    },
+    onoffLineListener : function () {
+        this.oHeader.classList[navigator.onLine?"remove":"add"]("offline");
+        this.DataManager = navigator.onLine? TODOOnline : TODOOffline;
+        this.DataManager.sync();
+        debugger;
     },
     build: function(context) {
     	var html = $("#entry-template").html();
@@ -65,51 +186,57 @@ var TODO = {
         return template(context);
     },
     get : function () {
-        TODOSync.get(function (json) {
-            $("#todo-list").append(json.map(function (obj) {
-                return TODO.build({target: obj.todo, key: obj.id});
-            }).join(""));
-            setTimeout(function () {
-                $("li").removeClass("appending");
-            }, 0);
-        })
+        this.DataManager.get(function (json) {
+            json.forEach(function (obj) {
+                var sTodo = TODO.build({target: obj.todo, key: obj.id});
+                TODO.oList.insertAdjacentHTML("beforeend", sTodo);
+            });
+            this.playAnimation();
+        }.bind(this))
     },
-    completed : function (target) {
-        var li = $(target).closest("li");
-        var checked = target.checked?"1":"0";
-
-        TODOSync.completed({
-            "key" : li.data("key"),
-            "completed" : checked
-        }, function () {
-            if(target.checked){
-                li.addClass("completed");
-            }else{
-                li.removeClass("completed");
-            }
-        })
-    },
-    remove : function (target) {
-        var li = $(target).closest("li")
-        li.one("transitionend", function () {
-            TODOSync.remove({
-                "key": li.data("key")
-            }, function () {
-                li.remove();
+    completed : function (e) {
+        if(e.target.classList.contains("toggle")){
+            var li = e.target.closest("li");
+            var data = { "key" : li.dataset.key, "completed" : e.target.checked?"1":"0" };
+            this.DataManager.completed(data, function () {
+                li.classList[e.target.checked?"add":"remove"]("completed");
             })
-        });
-        li.addClass("deleting");
+        };
+    },
+    remove : function (e) {
+        if(e.target.classList.contains("destroy")){
+            var li = e.target.closest("li");
+            if(li.classList.contains("deleting")){
+                return;
+            }
+            var data = {"key" : li.dataset.key};
+            var callback = function () {
+                this.DataManager.remove(data, function () {
+                    this.oList.removeChild(li);
+                }.bind(this));
+                li.removeEventListener("transitionend", callback);
+            }
+            li.addEventListener("transitionend", callback.bind(this));
+            li.classList.add("deleting");
+        }
     },
     add : function (e) {
-        if (e.keyCode !== this.ENTER_KEYCODE) {return}
-        var todo = TODO.target.value;
-        TODOSync.add(todo, function (json) {
-            $("#todo-list").prepend(TODO.build({target: todo, key: json.insertId}));
-            setTimeout(function () {
-                $("li").removeClass("appending");
-            }, 0);
-            TODO.target.value = ""; 
-        });
+        if (e.keyCode === this.ENTER_KEYCODE) {
+            var data = {todo: this.oInput.value};
+            this.DataManager.add(data, function (json) {
+                this.oList.insertAdjacentHTML('afterbegin', this.build({target: data.todo, key: json.insertId}));
+                this.playAnimation();
+                this.oInput.value = "";
+            }.bind(this));
+        }
+    },
+    playAnimation : function () {
+        setTimeout(function () {
+            var list = TODO.oList.querySelectorAll("li.appending");
+            for(var i=0; i<list.length; i++){
+                list[i].classList.remove("appending");
+            }
+        }, 0);
     }
 }
 
