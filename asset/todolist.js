c367ae49322e2f7c489f52c327277b8df21a3d89
030ii <lovelygeee2@gmail.com>
2015-08-04 15:18:12 +0900

    week2 주석 정리

--- asset/todolist.js
+++ asset/todolist.js
@@ -17,37 +17,14 @@ function addTODO(e){
 		var todo = makeTODO(document.getElementById("new-todo").value);
 
 		document.getElementById("todo-list").insertAdjacentHTML('beforeend', todo);
-		//document.getElementById("todo-list").appendChild(todo);
-
-		// todo.style.opacity = 0;
-		
-		// var i = 0;
-		// var key = setInterval(function(){
-		// 	if(i===50){
-		// 		clearInterval(key);
-		// 	} else {
-		// 		todo.style.opacity = 1 - i*0.02;
-		// 	}
-		// 	i++;
-		// },16);
-
-		/* 
-		 * - insertAdjacentHTML => 문자열 형태를 받아서 insert해준다. 
-		 * - appendChild => 노드가 인자로 들어가야한다.
-		 * 여기에서 todo는 string형태이기 때문에 insertAdjacentHTML을 사용했다.
-		 */
 		
 		// todo입력창에 쓴 내용 초기화 
 		document.getElementById("new-todo").value = "";
 	}
 }
 
-// e.currentTarget vs e.target
-
-
 // TODO를 완료(toggle을 클릭)했을 때 li에 completed 클래스 추가하는 함수
 function completedTODO(e){
-	//var input = e.currentTarget;
 	var input = e.target;
 	var li = input.parentNode.parentNode;
 
@@ -60,29 +37,13 @@ function completedTODO(e){
 
 // TODO를 삭제할 때의 함수 
 function removeTODO(e){
-	//var li = e.currentTarget.parentNode.parentNode;
 	var li = e.target.parentNode.parentNode;
 	
 	li.classList.add("deleting"); // 제대로 동작함
-	//$(li).addClass("deleting"); // classList.add 와 같은 동작 (나중에 한꺼번에 제이쿼리로 바꿀때 사용하자!)
-	//li.className = "removeTODO"; // fadeOut이 적용이 안됨
-
+	
 	setTimeout(function() {
         li.parentNode.removeChild(li);
     },1000);
-
-	// var i = 0;
-	// var key = setInterval(function(){
-	// 	if(i===50){
-	// 		clearInterval(key);
-	// 		li.parentNode.removeChild(li);
-	// 	} else {
-	// 		li.style.opacity = 1 - i*0.02;
-	// 	}
-	// 	i++;
-	// },16);
-
-	// setInterval vs requestAnimationFrame
 }
 
 
@@ -102,3 +63,15 @@ document.addEventListener("DOMContentLoaded", function(){
 		}
 	});
 });
+
+
+// --------wiki에 정리해볼것---------
+// insertAdjacentHTML vs appendChild
+/* 
+ * - insertAdjacentHTML => 문자열 형태를 받아서 insert해준다. 
+ * - appendChild => 노드가 인자로 들어가야한다.
+ * 여기에서 todo는 string형태이기 때문에 insertAdjacentHTML을 사용했다.
+ */
+
+// e.currentTarget vs e.target
+// setInterval vs requestAnimationFrame
