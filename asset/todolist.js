ee15b9fabd35a2e1eb3d0a35e106956541dffc40
030ii <lovelygeee2@gmail.com>
2015-08-14 20:49:01 +0900

    week3 object형으로 리팩토링

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,77 +1,62 @@
-// 핸드바 템플릿을 이용하여 TODO 엘리먼트를 만드는 함수
-function makeTODO(todo){
-	var source   = document.getElementById("todo-template").innerHTML;
-	var template = Handlebars.compile(source);
-	var context = {todoName: todo};
-	var todoTemplate = template(context);
+var TODO = {
+	init : function(){
+		document.addEventListener("DOMContentLoaded", function(){
+			document.getElementById("new-todo").addEventListener("keydown", this.add.bind(this)); // keypress로 해도됨! 
+			document.getElementById("todo-list").addEventListener("click", function(e){
+				if(e.target.className == "toggle"){
+					this.completed(e);
+				}
+				if(e.target.className == "destroy"){
+					this.remove(e);
+				}
+			}.bind(this));
+		}.bind(this));
+	},
 
-	return todoTemplate;
-}
+	makeTODO : function(todo){
+		var source   = document.getElementById("todo-template").innerHTML;
+		var template = Handlebars.compile(source);
+		var context = {todoName: todo};
+		var todoTemplate = template(context);
 
-// TODO를 추가하는 함수
-function addTODO(e){
-	var ENTER_KEYCODE = 13;
-	if(e.keyCode === ENTER_KEYCODE){ // 엔터를 눌렀을 때 
+		return todoTemplate;
+	},
 
-		// todo내용을 입력받아 todo 추가하기
-		var todo = makeTODO(document.getElementById("new-todo").value);
+	add : function(e){
+		var ENTER_KEYCODE = 13;
+		if(e.keyCode === ENTER_KEYCODE){ // 엔터를 눌렀을 때 
 
-		document.getElementById("todo-list").insertAdjacentHTML('beforeend', todo);
+			// todo내용을 입력받아 todo 추가하기
+			var todo = this.makeTODO(document.getElementById("new-todo").value);
+
+			document.getElementById("todo-list").insertAdjacentHTML('beforeend', todo);
+			
+			// todo입력창에 쓴 내용 초기화 
+			document.getElementById("new-todo").value = "";
+		}
+	},
+
+	completed : function(e){
+		var input = e.target;
+		var li = input.parentNode.parentNode;
+
+		if(input.checked){
+			li.className = "completed";
+		} else {
+			li.className = "";
+		}
+	},
+
+	remove : function(e){
+		var li = e.target.parentNode.parentNode;
 		
-		// todo입력창에 쓴 내용 초기화 
-		document.getElementById("new-todo").value = "";
+		li.classList.add("deleting"); // 제대로 동작함
+		
+		setTimeout(function() {
+	        li.parentNode.removeChild(li);
+	    },1000);
 	}
-}
+};
 
-// TODO를 완료(toggle을 클릭)했을 때 li에 completed 클래스 추가하는 함수
-function completedTODO(e){
-	var input = e.target;
-	var li = input.parentNode.parentNode;
+TODO.init();
 
-	if(input.checked){
-		li.className = "completed";
-	} else {
-		li.className = "";
-	}
-}
-
-// TODO를 삭제할 때의 함수 
-function removeTODO(e){
-	var li = e.target.parentNode.parentNode;
-	
-	li.classList.add("deleting"); // 제대로 동작함
-	
-	setTimeout(function() {
-        li.parentNode.removeChild(li);
-    },1000);
-}
-
-
-
-// Timer로 만든 애니메이션을 css3을 활용하여 변경할 것.
-// 	- css3에서 transition, transform, animation 공부하기
-// 완료하기랑 삭제하기에서 각각 붙였던 이벤트를 event delegate 기법을 활용하여 개선
-
-document.addEventListener("DOMContentLoaded", function(){
-	document.getElementById("new-todo").addEventListener("keydown", addTODO);
-	document.getElementById("todo-list").addEventListener("click", function(e){
-		if(e.target.className == "toggle"){
-			completedTODO(e);
-		}
-		if(e.target.className == "destroy"){
-			removeTODO(e);
-		}
-	});
-});
-
-
-// --------wiki에 정리해볼것---------
-// insertAdjacentHTML vs appendChild
-/* 
- * - insertAdjacentHTML => 문자열 형태를 받아서 insert해준다. 
- * - appendChild => 노드가 인자로 들어가야한다.
- * 여기에서 todo는 string형태이기 때문에 insertAdjacentHTML을 사용했다.
- */
-
-// e.currentTarget vs e.target
-// setInterval vs requestAnimationFrame
