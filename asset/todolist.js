50a42691ea8ef25927b0b7929cc1bd83b433ca25
Daybrush <itoolsg@gmail.com>
2016-06-24 15:49:53 +0900

    2week commit #108

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,5 +1,7 @@
 var ENTER_KEYCODE = 13;
-var SAMPLE_TODO = '<li class=""><div class="view"><input class="toggle" type="checkbox"><label>{title}</label><button class="destroy"></button></div></li>';
+var SAMPLE_TODO = '<li class="appending"><div class="view"><input class="toggle" type="checkbox"><label>{title}</label><button class="destroy"></button></div></li>';
+var listTodo = "";
+
 function changeTemplate(sample, data) {
 	var html = sample;
 	for(var key in data) {
@@ -7,15 +9,62 @@ function changeTemplate(sample, data) {
 	}
 	return html;
 }
+
+var _duration = 500;
+var _targetHeight = 58;
+var _startTime;
+var _appendTarget;
+function _addTodo() {
+	if(!_appendTarget)
+		return;
+		
+	var now = Date.now() - _startTime;
+	if(_duration <= now) {
+		_appendTarget.className = "";
+		_appendTarget.style.height = "";
+		_appendTarget = "";
+		return;
+	}
+	var height = parseInt(now/_duration * _targetHeight);
+	
+	_appendTarget.style.height = height + "px";
+	
+	requestAnimationFrame(_addTodo);
+}
+
 function addTodo(v) {
 	if(!v)
 		return;
-		
+	
 	var html = changeTemplate(SAMPLE_TODO, {title:v});
-	console.log(html);
-	var ul = document.getElementById("todo-list");
-	console.log(ul);
-	ul.insertAdjacentHTML("beforeend", html);
+	
+	listTodo.insertAdjacentHTML("afterbegin", html);
+	var li = listTodo.querySelector(".appending");
+	
+	_startTime = Date.now();
+	_appendTarget = li;
+	
+	_addTodo();
+}
+function completeTodo(li, checked) {
+	li.className = checked ? "completed" : "";
+}
+function _removeTodo(li) {
+	//li.parentNode.removeChild(li);
+	/* 	li.outerHTML = ""; */
+	li.remove();
+}
+function removeTodo(e) {
+	var li = e.target.parentNode.parentNode;
+	li.className += " deleting";
+	setTimeout(function() {
+		_removeTodo(li);
+	}, 1500);
+}
+function checkedTodo(e) {
+	var input = e.target;
+	var li = input.parentNode.parentNode;
+	completeTodo(li, input.checked);
 }
 document.addEventListener("DOMContentLoaded", function() {
 	var inputTodo = document.getElementById("new-todo");
@@ -27,4 +76,15 @@ document.addEventListener("DOMContentLoaded", function() {
 			inputTodo.value = "";
 		}
 	});	
+	listTodo = document.getElementById("todo-list");
+	listTodo.addEventListener("click", function(e) {
+		var target = e.target;
+		if(target) {
+			if(target.nodeName == "INPUT")
+				checkedTodo(e);
+			else if(target.nodeName == "BUTTON" && target.className == "destroy")
+				removeTodo(e);
+		}
+
+	});
 });
